0c064a7cc64254dfcc67d9b1604fafcfd6bb2c9a
NureSytnykPolina <polina.sytnyk@nure.ua>
2024-11-14 23:45:58 +0200

    Add files via upload

---
+++ LabTask1/android-pzpi-23-6-sytnyk-polina-lab-task1/app/src/main/java/com/example/myapplicationlab1/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplicationlab1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import android.util.Log;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            Log.d(TAG, "onCreate called");
+            return WindowInsetsCompat.CONSUMED; // Return the insets to consume them if desired
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart called");
+    }
+}
