fe9e84d0a85c6ac5aaf4db45dd5de30b80effdc4
Polina <polina.sytnyk@nure.ua>
2024-12-12 21:53:11 +0200

    Labtask1

--- app/src/main/java/nure/sytnyk/polina/labtask12/MainActivity.java
+++ app/src/main/java/nure/sytnyk/polina/labtask12/MainActivity.java
@@ -12,7 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
